95ebe3f021e11b190514ade5d5e5641c754cdf67
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-12-21 18:26:52 -0800

    simple pointer

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main(){
+
+	// Primmitive data
 	var i int
 	i = 42
 	fmt.Println(i)
@@ -24,4 +26,21 @@ func main(){
 
 	r, im := real(c), imag(c)
 	fmt.Println(r, im)
+
+	// Pointers
+
+	/*
+		setting up pointers by preceding with a *
+		deferennced pointers by preceding the var with an *
+	*/
+	var santaName *string = new(string)
+
+	// dereferencing
+	*santaName = "Santa Claus"
+
+	fmt.Println(santaName)
+	// output 0xc000088230 = reference to memory box
+
+	fmt.Println(*santaName)
+	// output is a pointer
 }
